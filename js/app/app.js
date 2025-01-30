340d335e17b0823060b1451fefaa189ae3d425fe
‘notilttoday’ <tsimbaloleg2006@gmail.com>
2025-01-30 18:26:59 +0200

    Day 2: first commit

--- js/app/app.js
+++ js/app/app.js
@@ -2,7 +2,7 @@ import { router } from './router.js';
 // import { header } from './widgets/header.js';
 // import { search } from './widgets/search.js';
 // import { popup } from './widgets/popup.js';
-// import { msg } from './widgets/msg.js';
+import { msg } from './widgets/msg.js';
 // import { toogle } from './widgets/toogle.js';
 // import { img } from './widgets/img.js';
 
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const main = {
         data() {
             return {
-                url: "",
+                url: "https://affiliate.yanbasok.com",
                 user: {name:"", phone:"", email:"", date:"", auth:""},
                 formData: {},
                 title: "",
@@ -43,9 +43,9 @@ document.addEventListener('DOMContentLoaded', function() {
                             self.page('/statistics');
                         } else if (['/statistics', '/payments', '/sites'].includes(self.$route['path']) && self.user.type == 'admin') {
                             self.page('/campaigns');
-                        } else if (['/campaigns', '/campaign', '/users', '/user', '/statistics', '/payments', '/sites'].includes(self.$route['path']) && self.user.type == 'admin') {
+                        } else if (['/campaigns', '/campaign', '/users', '/user', '/statistics', '/payments', '/sites'].includes(self.$route['path'])) {
                             self.page();
-                        } else if (!['/campaigns', '/campaign', '/users', '/user', '/statistics', '/payments', '/sites'].includes(self.$route['path']) && self.user.type == 'admin') {
+                        } else if (!['/campaigns', '/campaign', '/users', '/user', '/statistics', '/payments', '/sites'].includes(self.$route['path'])) {
                             self.page();
                         }
                     } else {
@@ -94,7 +94,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // .component('Header', header)
     // .component('popup', popup)
     // .component('search', search)
-    // .component('msg', msg)
+    .component('msg', msg)
     // .component('toogle', toogle)
     // .component('Image', img)
     .use(router)
--- js/app/pages/login.js
+++ js/app/pages/login.js
@@ -2,7 +2,6 @@ export const login = {
     data:function() {
         return {
             img: 1,
-            hs: 0,
             parent: ''
         }
     },
@@ -14,9 +13,68 @@ export const login = {
         randomIntFromInterval:function(min, max) {
             return Math.floor(Math.random() * (max - min + 1) + min)
         },
+        login:function() {
+            var self = this;
+            var data = self.parent.toFormData(self.parent.formData);
+
+            console.log(self.parent.toFormData(self.parent.formData));
+
+            axios.post(this.parent.url + "/site/login", data).then(function(response) {
+                if (response.data.error) {
+                    self.$refs.msg.alertFun(response.data.error);
+                }
+                if (response.data.user) {
+                    self.parent.user = response.data.user;
+                    self.parent.page('/campaigns');
+                    window.localStorage.setItem('user', JSON.stringify(self.parent.user));
+                }
+            }).catch(function(error) {
+                console.log('errors: ', error);
+            });
+        },
+
     },
     template: `
-        <div class="flex">
-            JKEJKEJF
+        <div class="flex page">
+            <msg ref="msg"/>
+            <div id="right-area" class="w40">
+                <div class="header">
+                    <div class="wrapper flex">
+                        <div class="logo">
+                            <img :src="parent.url+'/app/views/images/logo.svg'" />
+                        </div>
+                        <div class="al">
+                            <h1>Affiliate Sign in</h1>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="form inner-form flex">
+                    <form @submit.prevent="login()" v-if="parent.formData">
+                        <div class="row">
+                            <label>
+                                Email
+                                <input type="email" name="email" v-model="parent.formData.email" required>
+                            </label>
+                            
+                        </div>
+
+                        <div class="row">
+                            <label>
+                                Password
+                                <input type="password" name="password" v-model="parent.formData.password" required autocomplete="on">
+                            </label>
+                        </div>
+
+                        <div class="row">
+                            <button class="btn">SIGN IN</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            
+            <div id="left-area" class="w60">
+                <img :src="parent.url+'/app/views/images/Cover_'+img+'.jpg'" />
+            </div>
         </div>
 `};
