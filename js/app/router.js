7ec03a744238adfc2986843ca666bfaf4d0134ed
‘notilttoday’ <tsimbaloleg2006@gmail.com>
2025-02-09 01:24:24 +0200

    Day 6: first commit

--- js/app/router.js
+++ js/app/router.js
@@ -1,8 +1,8 @@
 import { login } from './pages/login.js';
 import { campaigns } from './pages/campaigns.js';
 import { campaign } from './pages/campaign.js';
-// import { users } from './pages/users.js';
-// import { user } from './pages/user.js';
+import { users } from './pages/users.js';
+import { user } from './pages/user.js';
 
 // import { ads } from './pages/ads.js';
 // import { statistics } from './pages/statistics.js';
@@ -15,8 +15,8 @@ export const router = VueRouter.createRouter({
         { path: "/", name: "Sign In",  component: login },
         { path: "/campaigns", name: "Campaigns",  component: campaigns },
         { path: "/campaign/:id", name: "Campaign",  component: campaign },
-        // { path: "/users", name: "Users",  component: users },
-        // { path: "/user", name: "User",  component: user },
+        { path: "/users", name: "Users",  component: users },
+        { path: "/user/:id", name: "User",  component: user },
         // { path: "/ads", name: "Ads",  component: ads },
         // { path: "/statistics", name: "Statistics",  component: statistics },
         // { path: "/payments", name: "Payments",  component: payments },
